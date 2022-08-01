28b6ed52cd2ab851ea7af308151f8a899e865b30
Nemo-Brunschwig <nemo.brunschwig@gmail.com>
2022-08-01 19:46:14 +0200

    optimisation des requêtes envoyées à bedetheque

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var divBD = document.querySelector("#BD"); // récupération de l'emplacement des BDs
 var divLoad = divBD.querySelector(".lds-ring"); // récupération de la div de chargement (pour la delete après)
-var allBDs = []; // tableau pour stocker les BDs (objets)
 var lucReq = new XMLHttpRequest(); // objet pour fair eles requêtes http
+if (localStorage['allBDs'] === undefined) {
+  // tableau pour stocker les BDs (objets)
+  localStorage['allBDs'] = "";
+  var allBDs = [];
+} else {
+  var allBDs = JSON.parse(localStorage['allBDs']);
+}
 
 lucReq.open("GET", "https://www.bedetheque.com/auteur-4544-BD-Brunschwig-Luc.html", true); // requête sur la page de Luc Brunschwig
 lucReq.responseType = "document"; // on veut une réponse de type "document"
@@ -18,50 +24,58 @@ lucReq.onload = async function(e) {
     // on stock tous les éléments dans une variable (au cas ou)
     var items = line.querySelectorAll("td");
     if (items[0].querySelector(".ico").querySelector("img").getAttribute("src").endsWith("France.png")) {
-      // on récupère le lien pour avoir des infos sur la BD en question
-      var link = items[0].querySelector(".serie").querySelector("a").getAttribute("href");
-      link = link.replace(".html", "__10000.html"); // on ajoute "__10000" pour afficher toutes les BDs
-      BD.link = link; // on remplit l'objet en ajoutant l'attribut "link"
 
       // on récupère son nom ça peut être bien
       var name = items[0].querySelector(".serie").innerText;
-      BD.name = name; // on ajoute l'attribut "name"
 
-      // on récupère la date de sortie (pour faire un trie plus tard)
-      var date = items[1].innerText;
-      date = date.replace(/\D/g, ""); // y avait un truc bizarre dans la string du coup je fais en sorte de récupérer que les chiffres
-      BD.date = parseInt(date, 10); // on ajoute l'attribut "date" converti en int
+      if (allBDs.find(bd => bd.name === name) !== undefined) {
+        BD = allBDs.find(bd => bd.name === name);
+      } else {
+        BD.name = name; // on ajoute l'attribut "name"
 
-      // récupération des données sur la BD
-      var bdReq = new XMLHttpRequest();
-      bdReq.open("GET", link, true);
-      bdReq.responseType = "document";
-      bdReq.onload = async function (e) {
-        var documentBD = bdReq.response; // page de la BD
-        var allNodes = Array.prototype.slice.call(documentBD.querySelector(".liste-albums").childNodes); // on récupère la liste des BDs \...
-        var listBDs = allNodes.filter(item => item.nodeName === "LI"); // .../
-        var linkImg = ""; // on prépare la variable
-        // on parcour la liste pour trouver celle qui a été faite par Luc (si c'est pas sa série, sinon on garde la première)
-        for (var i = 0; i < listBDs.length; i++) {
-          await until(_ => listBDs[i].querySelector(".infos") != null);
-          await until(_ => listBDs[i].querySelector(".infos").children.length > 0);
+        // on récupère le lien pour avoir des infos sur la BD en question
+        var link = items[0].querySelector(".serie").querySelector("a").getAttribute("href");
+        link = link.replace(".html", "__10000.html"); // on ajoute "__10000" pour afficher toutes les BDs
+        BD.link = link; // on remplit l'objet en ajoutant l'attribut "link"
 
-          if(listBDs[i].querySelector(".infos").innerText.includes("Brunschwig, Luc")){
-            // récupération de l'image de couv (du tome 1 ou du tome fait par Luc)
-            linkImg = listBDs[i].querySelector(".couv").querySelector("img").getAttribute("src");
-            break;
+        // on récupère la date de sortie (pour faire un trie plus tard)
+        var date = items[1].innerText;
+        date = date.replace(/\D/g, ""); // y avait un truc bizarre dans la string du coup je fais en sorte de récupérer que les chiffres
+        BD.date = parseInt(date, 10); // on ajoute l'attribut "date" converti en int
+
+        // récupération des données sur la BD
+        var bdReq = new XMLHttpRequest();
+        bdReq.open("GET", link, true);
+        bdReq.responseType = "document";
+        bdReq.onload = async function (e) {
+          var documentBD = bdReq.response; // page de la BD
+          var allNodes = Array.prototype.slice.call(documentBD.querySelector(".liste-albums").childNodes); // on récupère la liste des BDs \...
+          var listBDs = allNodes.filter(item => item.nodeName === "LI"); // .../
+          var linkImg = ""; // on prépare la variable
+          // on parcour la liste pour trouver celle qui a été faite par Luc (si c'est pas sa série, sinon on garde la première)
+          for (var i = 0; i < listBDs.length; i++) {
+            await until(_ => listBDs[i].querySelector(".infos") != null);
+            await until(_ => listBDs[i].querySelector(".infos").children.length > 0);
+
+            if(listBDs[i].querySelector(".infos").innerText.includes("Brunschwig, Luc")){
+              // récupération de l'image de couv (du tome 1 ou du tome fait par Luc)
+              linkImg = listBDs[i].querySelector(".couv").querySelector("img").getAttribute("src");
+              break;
+            }
           }
+          // await until(_ => linkImg != "");
+          linkImg = linkImg.replace("cache/thb_couv", "media/Couvertures"); // on arrange l'URL pour avoir l'image en grand
+          BD.img = linkImg; // on ajoute l'attribut "img"
+          allBDs.push(BD); // on ajoute la BD au tableau
         }
-        // await until(_ => linkImg != "");
-        linkImg = linkImg.replace("cache/thb_couv", "media/Couvertures"); // on arrange l'URL pour avoir l'image en grand
-        BD.img = linkImg; // on ajoute l'attribut "img"
-        allBDs.push(BD); // on ajoute la BD au tableau
+        bdReq.send();
       }
-      bdReq.send();
     }
   });
   // on attend que la liste soit remplie
   await until(_ => allBDs.length > 0);
+  // on enregistre la liste en local
+  localStorage["allBDs"] = JSON.stringify(allBDs);
   // tie de la liste par date
   allBDs.sort((a, b) => a.date - b.date);
   await allBDs.forEach(BD => {
