9283964e7d259022a0477b540920990546b67b9d
Nemo-Brunschwig <nemo.brunschwig@gmail.com>
2022-08-08 14:16:17 +0200

    on peut changer de thème :o

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ async function ShowBDs(series) {
     divModal.innerHTML += `<div class="modal fade" id="bd${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
       <div class="modal-dialog modal-xl modal-dialog-scrollable">
         <div class="modal-content bg-dark">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
           <div class="modal-body text-center">
             <h1 class="modal-title" id="staticBackdropLabel">${series[i].name}</h1>
             <div class="divider-custom">
