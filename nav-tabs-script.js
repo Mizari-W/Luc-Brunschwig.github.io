6a506dd927f0ac0aa19426738d26329760a1369c
Nemo-Brunschwig <nemo.brunschwig@gmail.com>
2022-08-13 03:26:28 +0200

    le drop down est plus jolie et s'adapte en fonction du thème

--- nav-tabs-script.js
+++ nav-tabs-script.js
@@ -15,6 +15,7 @@ async function Redim() {
     let items = dropdown.querySelectorAll("li");
     dropdown.innerHTML = "";
     await items.forEach(async item => {
+      await item.querySelector("button").setAttribute("class", `nav-link ${(item.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
       await navTab.append(item);
     });
     await navTab.append(dropdown);
@@ -24,17 +25,21 @@ async function Redim() {
     await Reset();
   } else if (window.matchMedia("(min-width: 1200px)").matches) {
     await Reset();
+    lmdlp.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(lmdlp.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
     dropdown.innerHTML = `
       <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Autres </a>
-      <ul class="dropdown-menu bg-dark">
+      <ul class="dropdown-menu ${localStorage['theme']==="darkTheme"?"bg-dark":"bg-light"}">
         ${lmdlp.outerHTML}
       </ul>`;
     lmdlp.parentNode.removeChild(lmdlp);
   } else if (window.matchMedia("(min-width: 992px)").matches) {
     await Reset();
+    lmdlp.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(lmdlp.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    urban.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(urban.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    sdc.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(sdc.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
     dropdown.innerHTML = `
       <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Autres </a>
-      <ul class="dropdown-menu bg-dark">
+      <ul class="dropdown-menu ${localStorage['theme']==="darkTheme"?"bg-dark":"bg-light"}">
         ${sdc.outerHTML}
         ${urban.outerHTML}
         ${lmdlp.outerHTML}
@@ -44,9 +49,13 @@ async function Redim() {
     sdc.parentNode.removeChild(sdc);
   } else if (window.matchMedia("(min-width: 768px)").matches) {
     await Reset();
+    lmdlp.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(lmdlp.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    urban.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(urban.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    sdc.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(sdc.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    makabi.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(makabi.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
     dropdown.innerHTML = `
       <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Autres </a>
-      <ul class="dropdown-menu bg-dark">
+      <ul class="dropdown-menu ${localStorage['theme']==="darkTheme"?"bg-dark":"bg-light"}">
         ${makabi.outerHTML}
         ${sdc.outerHTML}
         ${urban.outerHTML}
@@ -58,9 +67,15 @@ async function Redim() {
     makabi.parentNode.removeChild(makabi);
   } else if (window.matchMedia("(min-width: 576px)").matches) {
     await Reset();
+    lmdlp.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(lmdlp.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    urban.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(urban.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    sdc.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(sdc.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    makabi.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(makabi.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    holmes.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(holmes.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
+    mystery.querySelector("button").setAttribute("class", `dropdown-item ${localStorage['theme']==="darkTheme"?"text-white":""} ${(mystery.querySelector("button").getAttribute("class").includes("active")?"active":"")}`);
     dropdown.innerHTML = `
       <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Autres </a>
-      <ul class="dropdown-menu bg-dark">
+      <ul class="dropdown-menu ${localStorage['theme']==="darkTheme"?"bg-dark":"bg-light"}">
         ${mystery.outerHTML}
         ${holmes.outerHTML}
         ${makabi.outerHTML}
